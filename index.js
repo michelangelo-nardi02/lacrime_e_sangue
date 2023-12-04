8b5438413a79b158fa63c8fcb422efcc79d49d4c
Sofia Soldà <149075820+sofiasolda@users.noreply.github.com>
2023-12-04 20:50:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
-var EVENT_DOM_CONTENT_LOADED = 'DOMContentLoaded';
-
-function getGreetingId() {
-  return 'greeting';
-}
-
-function getGreeting() {
-  return "Una semplice estensione di prova, realizzata 'senza il minimo problema' e 'capendo tutto istantaneamente'. Ho trovato l'estensione originale su github -- Ho finalmente capito come funzioni manifest, e come si aggiungano estensioni a Chrome. Poco ma buono, direi";
-}
-
-function getGreetingElement() {
-  return document.getElementById(getGreetingId());
-}
-
-function renderGreeting() {
-  getGreetingElement().textContent = getGreeting();
-}
-//ma non è una ripetizione?
-
-function fireWhenDOMContentIsLoaded() {
-  renderGreeting();
-}
-// anche questa; non basta dirlo all'inizio del documento?
-document.addEventListener(EVENT_DOM_CONTENT_LOADED, fireWhenDOMContentIsLoaded);
-
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (changeInfo.status === 'complete') {
-    chrome.tabs.executeScript({
-      tabId: tabId,
-      code: 'document.body.textContent;'
-    }, function(results) {
-      console.log(results[0]);
-    });
-  }
+chrome.tabs.onUpdated.addListener(function (tabId, info) {
+    if(info.status === 'complete') {
+        chrome.tabs.executeScript({
+            code: "document.documentElement.innerHTML" // or 'file: "getPagesSource.js"'
+        }, function(result) {
+            if (chrome.runtime.lastError) {
+                console.error(chrome.runtime.lastError.message);
+            } else {
+                console.log(result)
+            }
+        });
+    }
 });
-
