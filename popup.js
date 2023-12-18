9af55f4c1f81c7a37a463c1786b37342e5d82138
giovannicerb <89905639+giovannicerb@users.noreply.github.com>
2023-12-18 23:22:25 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -6,8 +6,9 @@ chrome.tabs.query({ active: true, currentWindow: true },
   chrome.tabs.sendMessage(activeTab.id, { message: "get_html" }, function (response) {
     // Retrieves the HTML content of the response and sets it as the inner text of the element "htmlContent".
     var doc = document.getElementById("htmlContent")
+    // retrives the elements with the tag "a"
     var links = doc.getElementsByTagName("a")
-
+    // if there is at least one url  updates the textfirst anchor element with the HTML received from the content script
     if (links.length > 0) {
       links[0].innerText = response.html;
 
