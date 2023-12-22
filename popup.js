16ac9b0291c8f9eeb05593c9a8ae8143d979dd22
Sofia Soldà <149075820+sofiasolda@users.noreply.github.com>
2023-12-22 16:35:27 +0100

    Update popup.js; esperimento con var url

--- popup.js
+++ popup.js
@@ -3,6 +3,7 @@ chrome.tabs.query({ active: true, currentWindow: true },
   // this function retrives the open window with the index 0 and sends a message to obtain the html
   function (tabs) {
   var activeTab = tabs[0];
+  var url = activeTab.url;
   chrome.tabs.sendMessage(activeTab.id, { message: "get_html" }, function (response) {
     // <a href="www.domain.it">text message</a>
    var domains = [];
