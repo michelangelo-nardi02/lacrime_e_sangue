c619cb039f18d919aaa943b95e3961bedfcabdde
giovannicerb <89905639+giovannicerb@users.noreply.github.com>
2023-12-22 09:03:00 +0100

    Update popup.js
    Merely aligned every line to the left

--- popup.js
+++ popup.js
@@ -8,25 +8,25 @@ chrome.tabs.query({ active: true, currentWindow: true },
     //// da aggiungere tutti i giornali
    var domains = [];
 domains.push(["www.corriere.it", 1]);
-domains.push(["www.ilsole24ore.com", 2)];
-domains.push(["www.ilpost.it", 3)];
-domains.push(["www.laverità.info", 4)];
-domains.push(["www.internazionale.it", 5)];
-domains.push(["www.ilfoglio.it", 6)];
-domains.push(["www.unita.it", 7)];
-domains.push(["www.ilmanifesto.it", 8)];
-domains.push(["www.lespresso.it", 9)];
-domains.push(["www.lastampa.it", 10)]
+domains.push(["www.ilsole24ore.com", 2]);
+domains.push(["www.ilpost.it", 3]);
+domains.push(["www.laverità.info", 4]);
+domains.push(["www.internazionale.it", 5]);
+domains.push(["www.ilfoglio.it", 6]);
+domains.push(["www.unita.it", 7]);
+domains.push(["www.ilmanifesto.it", 8]);
+domains.push(["www.lespresso.it", 9]);
+domains.push(["www.lastampa.it", 10]);
 domains.push(["www.italiaoggi.it", 11]);
-domains.push(["www.avvenire.it", 12])
-  domains.push(["www.quotidiano.net", 13]);
-  domains.push(["www.ilmessaggero.it", 14]);
-   domains.push(["www.ilmattino.it", 15]);
-  domains.push(["www.ilrestodelcarlino.it", 16]);
-   domains.push(["www.liberoquotidiano.it", 17]);
-  domains.push(["www.repubblica.it", 18]);
-   domains.push(["www.ilgiornale.it", 19]);
-  domains.push(["www.ilfattoquotidiano.it", 20]);
+domains.push(["www.avvenire.it", 12]);
+domains.push(["www.quotidiano.net", 13]);
+domains.push(["www.ilmessaggero.it", 14]);
+domains.push(["www.ilmattino.it", 15]);
+domains.push(["www.ilrestodelcarlino.it", 16]);
+domains.push(["www.liberoquotidiano.it", 17]);
+domains.push(["www.repubblica.it", 18]);
+domains.push(["www.ilgiornale.it", 19]);
+domains.push(["www.ilfattoquotidiano.it", 20]);
     
     var ID = checkDomains(domains);
 
