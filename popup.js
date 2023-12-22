32d88acd1e1ca96b6b51daadb9a9584022e3ce90
Sofia Soldà <149075820+sofiasolda@users.noreply.github.com>
2023-12-22 09:01:29 +0100

    Update popup.js

--- popup.js
+++ popup.js
@@ -17,7 +17,18 @@ domains.push(["www.unita.it", 7)];
 domains.push(["www.ilmanifesto.it", 8)];
 domains.push(["www.lespresso.it", 9)];
 domains.push(["www.lastampa.it", 10)]
-var ID = checkDomains(domains);
+domains.push(["www.italiaoggi.it", 11]);
+domains.push(["www.avvenire.it", 12])
+  domains.push(["www.quotidiano.net", 13]);
+  domains.push(["www.ilmessaggero.it", 14]);
+   domains.push(["www.ilmattino.it", 15]);
+  domains.push(["www.ilrestodelcarlino.it", 16]);
+   domains.push(["www.liberoquotidiano.it", 17]);
+  domains.push(["www.repubblica.it", 18]);
+   domains.push(["www.ilgiornale.it", 19]);
+  domains.push(["www.ilfattoquotidiano.it", 20]);
+    
+    var ID = checkDomains(domains);
 
     console.log("Check links: " + response.links_page);
     document.getElementById("htmlContent").innerText = response.links_page;
