1b86cbd155d1f5efed2894dd3b6c74d88de0305d
giovannicerb <89905639+giovannicerb@users.noreply.github.com>
2024-01-03 16:55:25 +0100

    Add comments
    Added comments

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
-// provandoci di nuovo 
-
+// Individua scheda attiva, stora l'url nella var url
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   var activeTab = tabs[0];
   var url = activeTab.url;
-
+  
+// crea un array che contiene le coppie di domini e ID associati
    var domains = [];
 domains.push(["www.corriere.it", 1]);
 domains.push(["www.ilsole24ore.com", 2]);
@@ -25,10 +25,14 @@ domains.push(["www.liberoquotidiano.it", 17]);
 domains.push(["www.repubblica.it", 18]);
 domains.push(["www.ilgiornale.it", 19]);
 domains.push(["www.ilfattoquotidiano.it", 20]);
+domains.push(["www.panorama.it", 21]);
 
+// crea una funzione "checkDomains" per confrontare elementi di domains e l'url della pagina
   function checkDomains(domains, url) {
     for (var index = 0; index < domains.length; ++index) {
+      // con l'index 0 si ottiene il primo elemento della sottolista (ex. www.panorama.it)
       var domainString = domains[index][0];
+      //con l'index 1 si ottiene l'ID (ex. 21)
       var domainID = domains[index][1];
       if (url.includes(domainString)) {
         return domainID;
@@ -36,9 +40,10 @@ domains.push(["www.ilfattoquotidiano.it", 20]);
     }
     return -1;
   }
-
+// chiama la funzione "checkDomains" con la lista "domains" e "url". Il risultato è stored "domainID"
   const domainID = checkDomains(domains, url);
-
+  
+// se domainID è maggiore/uguale a zero viene stampato l'id, o una frase che spiega il contrario
   if (domainID >= 0) {
     console.log(`Corrispondenza trovata per il dominio con ID ${domainID}`);
     // Esegui l'azione relativa al dominio trovato qui
