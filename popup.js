f5bd625c69e7272911890d5a243253731d962308
Sofia Soldà <149075820+sofiasolda@users.noreply.github.com>
2024-01-19 17:00:59 +0100

    Update popup.js
    Now it (almost) works

--- popup.js
+++ popup.js
@@ -1,21 +1,7 @@
-//This JavaScript file content script performs a check to determine if the current webpage's domain 
-// matches any entries in the CSV file.
+// bene abbiamo copiato in alto checkDomains giusto per averla fuori da fetch
+// scusate i commenti deliranti ma funziona all'80%
 
-// Getting the CSV File
-// We are now retrieving the CSV file containing all the newspapers relevant for the present
-// version of the extention. 
-chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  var activeTab = tabs[0];
-  var url = activeTab.url;
-
-  //We are retreiving information about the current Chrome tab 
-
-        
-  console.log(url);
-  
-  // E FINO A QUI FUNZIONA
-
-   // We are creating the function "checkDomains" to compare elements of domains with the page's URL
+// We are creating the function "checkDomains" to compare elements of domains with the page's URL
     function checkDomains(container, url) {
       for (var index = 0; index < container.length; ++index) {
       // This is the link of the newspaper's home page (ex. www.panorama.it)
@@ -31,56 +17,76 @@ chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
        return -1;
     }
 
-// bene abbiamo copiato in alto checkDomains giusto per averla fuori da fetch
+chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  var activeTab = tabs[0];
+  var url = activeTab.url;
+
+  //We are retreiving information about the current Chrome tab 
+    console.log('Current URL:', url);
+  
+  // E FINO A QUI FUNZIONA
+
 // ora, prima di fetch mettiamo la richiesta alla query. AL POSTO DI FETCH perchè giustamente non abbiamo più il csv! 
 
+
 async function runQuery(sqlQuery) {
-	// BENE QUINDI QUI INCOLLIAMO IL SERVER DAL QUALE PRENDIAMO LA REICHIESTA HTTP 
-   const serverURL = ' http://nardinan.ddns.net:6664'; 
-   
-   
-// ABBIAMO un oggetto named options !! <3 :) 
-// a quanto pare la richiesta è di type POST e rimanda i dati in formato JSON, Includendo la SQL as part of the request body 
-   const options = {
-	   method: 'POST' ,
-	   headers: {
-		   'Content-Type': 'application/json',
-	   },	
-	body: JSON.stringify({	query: sqlQuery}),
-   };	
-		
+    // Log della chiamata a runQuery
 
-// Quindi ora si fetcha 
-// e si convertono le risposte in formato json
+    // BENE QUINDI QUI INCOLLIAMO IL SERVER DAL QUALE PRENDIAMO LA REICHIESTA HTTP 
+    const serverURL = 'http://nardinan.ddns.net:6664'; 
 
-	value = await fetch( serverURL, options)
-			.then(response => response.json())
-			.then ( data => {
-				return data;
-			})
-			
-// ora catchiamo gli errors
+    // Aggiungiamo un log per verificare l'URL del server
+    console.log('URL del server:', serverURL);
+	// questo funziona, me lo riporta nella console quindi top
 
-		.catch(error => {
-			console.error('There has been an issue following your request:' , error);
-		});
-		// ora prendiamo il value
-		return value
-	
+    // ABBIAMO un oggetto named options !! <3 :) 
+    // a quanto pare la richiesta è di type POST e rimanda i dati in formato JSON, Includendo la SQL as part of the request body 
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: sqlQuery }),
+    };
+
+    // Quindi ora si fetcha 
+    // e si convertono le risposte in formato json
+    value = await fetch(serverURL, options)
+        .then(response => response.json())
+        .then(data => {
+            return data;
+        })
+
+        // ora catchiamo gli errors
+        .catch(error => {
+            console.error('There has been an issue following your request:', error);
+        });
+
+    // Aggiungiamo anche un log prima del return, se necessario
+    console.log('Risultato della query:', value);
+
+    return value;
+}
+
 //trying our function, quindi selezioniamo link dalla table GIORNALI
 const queryDaEseguire = 'SELECT * FROM GIORNALI;' ;
 // ECCO
 runQuery(queryDaEseguire).then(result => {
+	   console.log('Result of the first query:', result);
+
 	var container = []
 	for ( var index = 0; index < result.rows; ++index) {
-		container[index] = []
-		container[index] [0] = result.query_result[index].LINK 
-		container[index][1] = result.query_result[index].IDGIORNALE
+		container[index] = [];
+		container[index][0] = result.query_result[index].LINK;
+		container[index][1] = result.query_result[index].IDGIORNALE;
 	}
+		
+	var primary_key = checkDomains (container, url);
+	console.log('This newspaper is in our database, with ID: ', checkDomains(container, url));
 	
-	
-	primary_key = checkDomains (container, url )
 	if (primary_key >= 0) {
+// bene raga per qualche motivo non vanno nè estrema destra nè nessun orientamento, e mostra estrema sinistra la posto di ES!!
+// ma il risultato di queste 2 ore è che va quasi tutto 
 	const anotherQuery = " SELECT G. LINK, " + 
 		" (CASE GREATEST(G.ES, G.S, G.CS, G.C, G.CD, G.D, G.ED, G.NO) " +
 		"	 WHEN G.ES THEN 'Estrema Sinistra' " +
@@ -88,14 +94,17 @@ runQuery(queryDaEseguire).then(result => {
 		"	 WHEN G.CS THEN 'Centro Sinistra' " + 	
 		"	 WHEN G.C THEN 'Centro' " + 	
 		"	 WHEN G.CD THEN 'Centro Destra' " + 	
-		"	 WHEN G.ED	 THEN 'Estrema Destra' " + 	
+		"	 WHEN G.ED THEN 'Estrema Destra' " + 	
 		"	 WHEN G.NO THEN 'Nessun Orientamento' " + 	
 		" END) AS OrientamentoPrincipale" +
-		" FROM" +
+		" FROM " +
 		" GIORNALI G" +
 		" WHERE" + 
 		" IDGIORNALE = " + primary_key.toString()
+		
+		
 	runQuery(anotherQuery).then(lastResult => {
+		      console.log('Result of the second query:', lastResult);
 		if (lastResult.rows > 0) {
 			document.getElementById("htmlContent").innerText = lastResult.query_result[0].OrientamentoPrincipale
 		} else {
@@ -104,5 +113,4 @@ runQuery(queryDaEseguire).then(result => {
 	});
 	}
 });
-}
 })
