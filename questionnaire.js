cfed94e2b5d0c47c5fb3715e5bf30ce2a451aba3
giovannicerb <89905639+giovannicerb@users.noreply.github.com>
2024-04-17 12:32:51 +0200

    Update comments

--- questionnaire.js
+++ questionnaire.js
@@ -1,4 +1,4 @@
-// questionnaire.js
+// This code creates an event listener that opens the questionnaire once the html element questionnaireButton is clicked
 document.getElementById('questionnaireButton').addEventListener('click', function() {
     chrome.tabs.create({ url: 'questionnaire.html' });
 });
