1b5380c35b66529086b6fdc299b2350c70fb0cf0
Sofia Soldà <149075820+sofiasolda@users.noreply.github.com>
2024-06-16 18:11:16 +0200

    Update security.js
    fixed comments

--- security.js
+++ security.js
@@ -1,10 +1,9 @@
-// security.js
 
-// Show questionnaire expand button on click
+// Show questionnaire expand button when clicking the button
 document.getElementById('results_button').addEventListener('click', function() {
     document.getElementById('questionnaire_expand').style.display = 'block';
   });
-  // Show questionnaire button on click
+  // Showing the button linking to our questionnaire
   document.getElementById('questionnaire_expand').addEventListener('click', function() {
     document.getElementById('questionnaireButton').style.display = 'block';
   });
